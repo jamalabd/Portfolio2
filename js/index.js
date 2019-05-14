bfa0fe563cd45543a76f968378e1e33c5b62ec55
Jamal Abdul-majid <jamalabdul-majid@Jamals-MacBook-Pro.local>
2019-05-14 16:39:03 -0400

    resume needs to added to sight

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+  // nav link shadow
   $('.navbar-nav').on('click','li', function(){
       $('.navbar-nav li.grey').removeClass('grey');
       $(this).addClass('grey');
   });
+
+  $('.pr').hover(function() {
+    $('.popup',this).css('display','flex');
+  },function() {
+    $('.popup',this).css('display','none');
+
+  });
+
 });

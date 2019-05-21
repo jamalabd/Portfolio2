679ba416976b4a4a7309ccca914ae824a9c9367a
Jamal Abdul-majid <jamalabdul-majid@Jamals-MacBook-Pro.local>
2019-05-21 15:41:51 -0400

    working on hamburger meue

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-  // nav link shadow
+// nav link shadow
   $('.navbar-nav').on('click','li', function(){
       $('.navbar-nav li.grey').removeClass('grey');
       $(this).addClass('grey');
+
   });
 
+
+// NavBar Hamburger Menu
+ $('.hamburger-wrapper').on('click', ()=> {
+   $('.navbar-toggler').toggleClass('close');
+ });
+
+
+
+// Project Section
   $('.pr').hover(function() {
     $('.popup',this).css('display','flex');
   },function() {
     $('.popup',this).css('display','none');
 
   });
+
 
 });

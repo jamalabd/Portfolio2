ec4d8c7c3fcec91430b6fded22902cebfe63fca2
Jamal Abdul-majid <jamalabdul-majid@Jamals-MacBook-Pro.local>
2019-06-24 13:42:53 -0400

    working on rendering popup

--- js/index.js
+++ js/index.js
@@ -23,5 +23,56 @@ $(document).ready(function(){
 
   });
 
+  // contact section forms and buttons
+
+  // variables
+
+  const status = $('.status');
+  status.innerHTML="";
+
+  $('#submit-btn').click(function() {
+    $('.formContact').validate({
+      rules: {
+        email: {required: true, email: true},
+        name: {required: true, lettersonly: true, name: true},
+        phone: {required: true, phoneUs: true, name: true},
+        message: {required: true, name: true}
+
+      },
+      messages: {
+       email: {required: 'email cant be blank..', email: 'email is not valid..'},
+       name: {required: 'name cant be blank..', name: 'name is not valid..'},
+       phone: {required: 'phone cant be blank..', phone: 'phone is not valid..'},
+       message: {required: 'message cant be blank..', name: 'message is not valid..'}
+     },
+
+     errorPlacement: function(error, element){
+       if (error) {
+         error.insertAfter('.formContact');
+       }
+     }
+
+    });
+
+  });
+
+
+  const popUp=()=>{
+    markup =
+    `
+    <div class="viewMoreWindow window1">
+      <img class="projectImg" src="" alt="">
+      <h5 class="projectTitle"></h5>
+      <p class="description"></p>
+      <button class="viewSight" type="button" name="button"></button>
+      <button class="viewCode" type="button" name="button"></button>
+    </div>
+    `;
+
+    document.querySelector('.windows').insertAdjacentHTML('beforeend', markup);
+  };
+
+
+popUp();
 
 });
